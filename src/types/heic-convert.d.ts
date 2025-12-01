206b3e7bfbdb7850d97f4a0452e81eb7274c6f30
Gabby Welson <gabby@welson.net>
2025-11-30 22:15:51 -0800

    Upload endpoint

---
+++ src/types/heic-convert.d.ts
@@ -0,0 +1,14 @@
+declare module "heic-convert" {
+  interface ConvertOptions {
+    buffer: Buffer;
+    format: "PNG" | "JPEG";
+    quality?: number;
+  }
+
+  interface ConvertResult {
+    (options: ConvertOptions): Promise<Uint8Array>;
+  }
+
+  const convert: ConvertResult;
+  export default convert;
+}
